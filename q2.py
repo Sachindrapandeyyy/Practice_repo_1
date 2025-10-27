69bff6c62e9d88a9464aba08f83e6569920fc09d
Sachindrapandeyyy <Sachindrapandey328@gmail.com>
2025-10-27 16:52:07 +0530

    q2 done

---
+++ q2.py
@@ -0,0 +1,5 @@
+#Simple Sum Do numbers lo (int), unka sum calculate karo, aur result print ka
+a = int(input("Pehla number daalo: "))
+b = int(input("Doosra number daalo: "))
+sum = a + b
+print("Dono numbers ka sum hai:", sum)
